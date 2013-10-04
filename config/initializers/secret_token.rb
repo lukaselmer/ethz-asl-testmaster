dc45060ec79de08fcc15dc0dd7fc50b6195a134c
Lukas Elmer <lukas.elmer@gmail.com>
2013-10-05 00:20:28 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EthzAslTestmaster::Application.config.secret_key_base = '28420dc03c0d95982b888635b627b3b0594c82c6c8fd14fbd0e151c097ffbb44fa2767c875fe7ff563d679efd34770f4fea4602cecbe56b5406e5247b6d49234'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EthzAslTestmaster::Application.config.session_store :cookie_store, key: '_ethz-asl-testmaster_session'
