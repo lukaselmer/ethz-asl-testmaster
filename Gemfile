b48b65bf028dd99ad96578fad1a4326f0a50d774
Lukas Elmer <lukas.elmer@gmail.com>
2013-10-12 02:31:45 +0200

    code climate, code coverage

--- Gemfile
+++ Gemfile
@@ -45,4 +45,5 @@ group :test do
   gem 'capybara'
   gem 'database_cleaner', '1.0.1'
   gem 'email_spec'
+  gem 'codeclimate-test-reporter', require: nil
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'email_spec'
@@ -7,7 +13,7 @@ require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join('spec/support/**/*.rb')].each {|f| require f}
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.include(EmailSpec::Helpers)
