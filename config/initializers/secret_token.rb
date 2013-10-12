449c4fb4b2c9323c47d2c31650aba40acd5fe895
Lukas Elmer <lukas.elmer@gmail.com>
2013-10-12 03:32:15 +0200

    move secret key base to config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-EthzAslTestmaster::Application.config.secret_key_base = '28420dc03c0d95982b888635b627b3b0594c82c6c8fd14fbd0e151c097ffbb44fa2767c875fe7ff563d679efd34770f4fea4602cecbe56b5406e5247b6d49234'
+EthzAslTestmaster::Application.config.secret_key_base = ENV['RAILS_SECRET_KEY_BASE']
