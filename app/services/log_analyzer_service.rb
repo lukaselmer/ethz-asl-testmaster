f5e5931a3a5cbcf1c249e07de4b95c154720400c
Lukas Elmer <lukas.elmer@gmail.com>
2013-11-07 07:06:46 +0100

    bugfix

--- app/services/log_analyzer_service.rb
+++ app/services/log_analyzer_service.rb
@@ -29,7 +29,7 @@ class LogAnalyzerService
     params = "-d #{c.collected_logs_path} -type #{type} -fmt #{output_format} -w #{window_size} > #{outfile}"
     @jar_executor.execute_log_analyzer params
 
-    if output_format.start_with? 'Gnu-'
+    if output_format.start_with? 'gnu-'
       ext = output_format.split('-')[1]
       img_outfile = c.analyzer_out_file(ext)
       @cmd_executor.exec!("gnuplot #{outfile} > #{img_outfile}")
