395e1d1c1145051b8d695f944a6c80b6743188f4
Lukas Elmer <lukas.elmer@gmail.com>
2013-11-07 07:13:38 +0100

    high stock error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,6 @@
 //= require cocoon
 //= require highcharts/highcharts
 //= require highcharts/highcharts-more
-//= require highcharts/highstock
 //= require_tree .
+
+// require highcharts/highstock
