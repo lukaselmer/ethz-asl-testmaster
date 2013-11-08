0c5e682fc751ff2d308157eb163de396a4d1be84
Lukas Elmer <lukas.elmer@gmail.com>
2013-11-08 21:02:52 +0100

    Update scenario.rb

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -2,7 +2,8 @@ class Scenario < ActiveRecord::Base
   belongs_to :test_run
   has_many :scenario_executions
 
-  validates :execution_multiplicity, :execution_multiplicity_per_machine, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }, if: -> (s) { s.test_run }
+  validates :execution_multiplicity, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }, if: -> (s) { s.test_run }
+  validates :execution_multiplicity_per_machine, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 500 }, if: -> (s) { s.test_run }
   validates :name, :scenario_type, presence: true
   validates :scenario_type, presence: true, inclusion: %w(client broker)
 
