d2a65f42f916a8ee5212d864c1dc38367e40ec8a
Lukas Elmer <lukas.elmer@gmail.com>
2013-11-13 03:29:32 +0100

    cron

--- app/services/analyze_cron.rb
+++ app/services/analyze_cron.rb
@@ -1,10 +1,16 @@
 class AnalyzeCron
   def initialize
-    @ended_tests = TestRun.all.to_a.select { |test_run| test_run.ended? }
+    @ended_tests = ended_tests
+  end
+
+  def ended_tests
+    TestRun.all.to_a.select { |test_run| test_run.ended? }
   end
 
   def run
     @ended_tests.each do |t|
+      return if @ended_tests.size <= ended_tests.size
+
       %w(BTotReqResp).each do |message_type|
         other = {'startup_cooldown_time' => 240000, 'message_type' => message_type}
 
