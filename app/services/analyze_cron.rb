87a090ae137ce08b1a815ce663ec73a42949e562
Lukas Elmer <lukas.elmer@gmail.com>
2013-11-13 02:08:20 +0100

    analyze cron

--- app/services/analyze_cron.rb
+++ app/services/analyze_cron.rb
@@ -7,10 +7,12 @@ class AnalyzeCron
     @ended_tests.each do |t|
       %w(BTotReqResp).each do |message_type|
         other = {'startup_cooldown_time' => 240000, 'message_type' => message_type}
-        output_format = 'txt'
 
         l = LogAnalyzerService.new
-        l.analyze(t, 'txt', 1000, other)
+
+        puts "Analyzing test run #{t.id} with message type #{message_type}..."
+        f = l.analyze(t, 'txt', 1000, other)
+        puts "Done analyzing test run #{t.id} with message type #{message_type}, output file #{f}"
       end
     end
   end
