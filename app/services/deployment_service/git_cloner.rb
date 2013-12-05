5b39465f88232434c316cec3d22936b763c36ccb
Lukas Elmer <lukas.elmer@gmail.com>
2013-12-05 15:29:32 +0100

    disable microbenchmarks

--- app/services/deployment_service/git_cloner.rb
+++ app/services/deployment_service/git_cloner.rb
@@ -10,6 +10,7 @@ class DeploymentService::GitCloner
   end
 
   def clone_git
-    @cmd_executor.exec! "git clone -b microbenchmarks git@github.com:ganzm/AdvancedSystemsLab2013.git #{git_path}"
+    #@cmd_executor.exec! "git clone -b microbenchmarks git@github.com:ganzm/AdvancedSystemsLab2013.git #{git_path}"
+    @cmd_executor.exec! "git clone git@github.com:ganzm/AdvancedSystemsLab2013.git #{git_path}"
   end
 end
